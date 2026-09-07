5e886ddc1bcc49cb2df36777f6f85bf8ee3bfb29
unknown <ashwin_zapuza@yahoo.com>
2026-09-07 20:59:21 +0530

    Add JWT authentication and API routing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'banking',
     'rest_framework',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
